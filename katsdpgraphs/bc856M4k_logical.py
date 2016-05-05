5b815335c75b011b365ae3adb96d7a4b30b101be
Bruce Merry <bmerry@ska.ac.za>
2016-05-05 18:32:45 +0200

    Fix typos that prevented the script running
    It now works, although to capture the full band it's necessary to change
    the multicast routing on the BFI machine to swap the polls between the
    NICs, otherwise everything is criss-crossing the NUMA nodes. That should
    probably be fixed by SR-441.
    
    As expected, capture-done is timing out.

--- katsdpgraphs/bc856M4k_logical.py
+++ katsdpgraphs/bc856M4k_logical.py
@@ -37,8 +37,8 @@ def build_physical_graph(r):
      # ingest node for ar1
 
     for i in range(2):
-        G.add_node('sdp.bf_ingest.{}'.format(i+1),{'port': r.get_port('sdp_bf_ingest_{}_katcp').format(i+1), 'file_base':'/data', 'cbf_channels': 4096, 'affinity': [i+2, i+4, i+6], 'buffer': True\
-             'docker_image':r.get_image_path('katsdpingest:bf-ingest'),'docker_host_class':'bf_ingest', 'docker_cmd':'bf_ingest.py',\
+        G.add_node('sdp.bf_ingest.{}'.format(i+1),{'port': r.get_port('sdp_bf_ingest_{}_katcp'.format(i+1)), 'file_base':'/data', 'cbf_channels': 4096, 'affinity': [i+2, i+4, i+6], 'buffer': True,\
+             'docker_image':r.get_image_path('katsdpingest'),'docker_host_class':'bf_ingest', 'docker_cmd':'bf_ingest.py',\
              'docker_params': {"network":"host", "binds": {"/var/kat/data/pol{}".format(i):{"bind":"/data","ro":False}}},\
              'state_transitions':{2:'capture-init',5:'capture-done'}\
             })
