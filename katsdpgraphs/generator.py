519c8ab0dacff3e5d8fb64d326cd6ff882d6c31a
Bruce Merry <bmerry@ska.ac.za>
2016-06-07 15:59:56 +0200

    Unify all the graphs into one function
    This does cause some changes to some of the graphs, where unwanted
    variation had crept in:
    
    1. Some graphs are missing the sdp.ingest1 -> sdp.timeplot.1 edge.
    Resolution: add to all
    
    2. Some graphs do not pass spead_port to timeplot. Resolution: add to
    all.
    
    3. Some graphs (I think the sim graphs) hardcode the camtospead port
    as :7147. Resolution: change all to use CAM_spead multicast stream
    (I'm assuming this is harmless even if kattelmod doesn't supply it).
    
    4. The c856M32k_sim graph uses c856M4k_spead for the sim -> ingest
    edge. Changing to c856M32k_spead.
    
    5. bc856M4ksim seems to be completely missing the beamformer node and
    edge from CBF. Adding in.
    
    6. bc856M4ksim seems to have a completely different configuration for
    timeplot. Replacing with the version used in other graphs.
    
    7. Removed output_int_time and sd_int_time from graphs that had it
    (they is now computed).

--- katsdpgraphs/generator.py
+++ katsdpgraphs/generator.py
@@ -1,22 +1,31 @@
+from __future__ import print_function, division, absolute_import
 import networkx as nx
+import re
 
-def define_hosts():
-     # in the future this will be discovered at master controller boot time
-     # by some form of zeroconf discovery.
-    available_hosts = {'sdp-ingest1.mkat':\
-                          {'ip':'127.0.0.1','host_class':'nvidia_gpu'},
-                       'sdp-spdmc.mkat':\
-                          {'ip':'127.0.0.1','host_class':'sdpmc'}}
-    return available_hosts
+def build_physical_graph(beamformer_mode, cbf_channels, simulate, resources):
+    """Generate a logical graph.
 
-def build_physical_graph(r):
-
+    Parameters
+    ----------
+    beamformer_mode : {"none", "hdf5", "ptuse"}
+        Which nodes (if any) to provide for beamformer capture
+    channels : int
+        Number of correlator channels
+    simulate : boolean
+        If true, run the CBF correlator simulator
+    resources : :class:`katsdpcontroller.SDPResources`
+        Resources for the graph
+    """
+    if beamformer_mode not in ['none', 'ptuse', 'hdf5']:
+        raise ValueError('Unrecognised beamformer_mode ' + beamformer_mode)
+    r = resources
+    c_stream = 'c856M{}k_spead'.format(cbf_channels // 1024)
     telstate = '{}:{}'.format(r.get_host_ip('sdpmc'), r.get_port('redis'))
 
     G = nx.DiGraph()
 
      # top level attributes of this graph, used by all nodes
-    attributes = {'sdp_cbf_channels': 4096,
+    attributes = {'sdp_cbf_channels': cbf_channels,
                   'cal_refant':'', 'cal_g_solint':10, 'cal_bp_solint':10, 'cal_k_solint':10, 'cal_k_chan_sample':10}
 
     G.graph.update(attributes)
@@ -28,7 +37,7 @@ def build_physical_graph(r):
         {"port_bindings":{6379:r.get_port('redis')}}, 'docker_host_class':'sdpmc'})
      # launch redis node to hold telescope state for this graph
 
-    G.add_node('sdp.ingest.1',{'port': r.get_port('sdp_ingest_1_katcp'), 'sd_int_time':2.0, 'output_int_time':2.0, 'antennas':4, 'continuum_factor': 32, 'sd_continuum_factor': 16, 'cbf_channels': 4096,\
+    G.add_node('sdp.ingest.1',{'port': r.get_port('sdp_ingest_1_katcp'), 'antennas':4, 'continuum_factor': 32, 'sd_continuum_factor': cbf_channels // 256, 'cbf_channels': cbf_channels,\
          'docker_image':r.get_image_path('katsdpingest_titanx'),'docker_host_class':'nvidia_gpu', 'docker_cmd':'ingest.py',\
          'docker_params': {"network":"host", "devices":["/dev/nvidiactl:/dev/nvidiactl",\
                           "/dev/nvidia-uvm:/dev/nvidia-uvm","/dev/nvidia0:/dev/nvidia0"]},\
@@ -36,15 +45,16 @@ def build_physical_graph(r):
         })
      # ingest node for ar1
 
-    G.add_node('sdp.bf_ingest.1',{'port': r.get_port('sdp_bf_ingest_1_katcp'), 'cbf_channels': 4096, \
-         'docker_image':r.get_image_path("katsdpingest"),'docker_host_class':'bf_ingest', 'docker_cmd':'taskset 0,2,4,6 ptuse_ingest.py',\
-         'docker_params': {"network":"host", "binds": {"/scratch":{"bind":"/data","ro":False}},
-             "devices":["/dev/nvidiactl:/dev/nvidiactl","/dev/nvidia-uvm:/dev/nvidia-uvm","/dev/nvidia0:/dev/nvidia0"],\
-             "privileged":True, "ulimits":[{"Name":"memlock","Soft":33554432,"Hard":-1}],\
-             "ipc_mode":"host"},\
-         'state_transitions':{2:'capture-init',5:'capture-done'}\
-        })
-     # beamformer ingest node for ar1
+    if beamformer_mode == 'ptuse':
+        G.add_node('sdp.bf_ingest.1',{'port': r.get_port('sdp_bf_ingest_1_katcp'), 'cbf_channels': cbf_channels, \
+             'docker_image':r.get_image_path("katsdpingest"),'docker_host_class':'bf_ingest', 'docker_cmd':'taskset 0,2,4,6 ptuse_ingest.py',\
+             'docker_params': {"network":"host", "binds": {"/scratch":{"bind":"/data","ro":False}},
+                 "devices":["/dev/nvidiactl:/dev/nvidiactl","/dev/nvidia-uvm:/dev/nvidia-uvm","/dev/nvidia0:/dev/nvidia0"],\
+                 "privileged":True, "ulimits":[{"Name":"memlock","Soft":33554432,"Hard":-1}],\
+                 "ipc_mode":"host"},\
+             'state_transitions':{2:'capture-init',5:'capture-done'}\
+            })
+         # beamformer ingest node for ar1
 
 
     G.add_node('sdp.filewriter.1',{'port': r.get_port('sdp_filewriter_1_katcp'),'file_base':'/var/kat/data',\
@@ -57,9 +67,20 @@ def build_physical_graph(r):
 
     G.add_node('sdp.cal.1',{'docker_image':r.get_image_path('katsdpcal'),'docker_host_class':'nvidia_gpu', 'docker_cmd':'run_cal.py',\
                'docker_params': {"network":"host","volumes":"/var/kat/data",\
-                 "binds": {"/var/kat/data":{"bind":"/var/kat/data","ro":False}}}, 'cbf_channels': 4096, \
+                 "binds": {"/var/kat/data":{"bind":"/var/kat/data","ro":False}}}, 'cbf_channels': cbf_channels, \
               })
      # calibration node
+
+    if simulate:
+        # create-fx-product is passed on the command-line insteead of telstate
+        # for now due to SR-462.
+        G.add_node('sdp.sim.1',{'port': r.get_port('sdp_sim_1_katcp'), 'cbf_channels': cbf_channels,
+             'docker_image':r.get_image_path('katcbfsim'),'docker_host_class':'nvidia_gpu', 'docker_cmd':'cbfsim.py --create-fx-product ' + r.prefix,\
+             'docker_params': {"network":"host", "devices":["/dev/nvidiactl:/dev/nvidiactl",\
+                              "/dev/nvidia-uvm:/dev/nvidia-uvm","/dev/nvidia0:/dev/nvidia0"]}
+            })
+
+     # simulation node
 
     G.add_node('sdp.timeplot.1',{'spead_port': r.get_port('spead_sdisp'), 'html_port': r.get_sdisp_port_pair('timeplot')[0],\
          'capture_server': '{}:{}'.format('127.0.0.1', r.get_port('sdp_ingest_1_katcp')),\
@@ -75,15 +96,17 @@ def build_physical_graph(r):
     G.add_edge('sdp.telstate','sdp.cal.1',{'telstate': telstate})
      # connections to the telescope state. 
 
-    G.add_edge('cbf.output.1','sdp.ingest.1',{'cbf_spead':'{}:{}'.format(r.get_multicast_ip('c856M4k_spead'), r.get_port('c856M4k_spead'))})
+    c_node = 'sdp.sim.1' if simulate else 'cbf.output.1'
+    G.add_edge(c_node,'sdp.ingest.1',{'cbf_spead':'{}:{}'.format(r.get_multicast_ip(c_stream), r.get_port(c_stream))})
      # spead data from correlator to ingest node
 
     G.add_edge('cam.camtospead.1','sdp.ingest.1',{'cam_spead':'{}:{}'.format(r.get_multicast_ip('CAM_spead'),r.get_port('CAM_spead'))})
      # spead data from camtospead to ingest node. For simulation this is hardcoded, as we have no control over camtospead
 
-    G.add_edge('cbf.bf_output.1','sdp.bf_ingest.1',{'cbf_speadx':'{}:{}'.format(r.get_multicast_ip('beam_0x_spead'),r.get_port('beam_0x_spead')),
-                                                    'cbf_speady':'{}:{}'.format(r.get_multicast_ip('beam_0y_spead'),r.get_port('beam_0y_spead'))})
-     # spead data from beamformer to ingest node
+    if beamformer_mode == 'ptuse':
+        G.add_edge('cbf.bf_output.1','sdp.bf_ingest.1',{'cbf_speadx':'{}:{}'.format(r.get_multicast_ip('beam_0x_spead'),r.get_port('beam_0x_spead')),
+                                                        'cbf_speady':'{}:{}'.format(r.get_multicast_ip('beam_0y_spead'),r.get_port('beam_0y_spead'))})
+         # spead data from beamformer to ingest node
 
     G.add_edge('sdp.ingest.1','sdp.cal.1',{'l0_spectral_spead':'{}:{}'.format(r.get_multicast_ip('l0_spectral_spead'), r.get_port('l0_spectral_spead'))})
      # ingest to cal node transfers L0 visibility data (no calibration)
@@ -98,3 +121,25 @@ def build_physical_graph(r):
      # cal to file writer transfers L1 visibility data (cal tables applied)
 
     return G
+
+def graph_parameters(graph_name):
+    """Convert a product name into a dictionary of keyword parameters to pass to
+    :func:`build_physical_graph`.
+
+    Parameters
+    ----------
+    graph_name : str
+        Logical graph name such as ``bc856M4k``
+
+    Returns
+    -------
+    parameters : dict
+        Key-value pairs to pass as parameters to :func:`build_physical_graph`
+    """
+    match = re.match('^(c|bc|bec)856M(4|32)k((?:sim)?)$', graph_name)
+    if not match:
+        raise ValueError('Unsupported graph ' + product)
+    beamformer_modes = {'c': 'none', 'bc': 'ptuse', 'bec': 'hdf5'}
+    return dict(beamformer_mode=beamformer_modes[match.group(1)],
+                cbf_channels=int(match.group(2)) * 1024,
+                simulate=bool(match.group(3)))
