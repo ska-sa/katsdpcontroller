cc6f631d0781def230fcd03ae9ad065c400dea95
Bruce Merry <bmerry@ska.ac.za>
2016-06-08 14:55:59 +0200

    Run ptuseingest using 'beamform' image name

--- katsdpgraphs/generator.py
+++ katsdpgraphs/generator.py
@@ -47,7 +47,7 @@ def build_physical_graph(beamformer_mode, cbf_channels, simulate, resources):
 
     if beamformer_mode == 'ptuse':
         G.add_node('sdp.bf_ingest.1',{'port': r.get_port('sdp_bf_ingest_1_katcp'), 'cbf_channels': cbf_channels, \
-             'docker_image':r.get_image_path("katsdpingest"),'docker_host_class':'bf_ingest', 'docker_cmd':'taskset 0,2,4,6 ptuse_ingest.py',\
+             'docker_image':r.get_image_path("beamform"),'docker_host_class':'bf_ingest', 'docker_cmd':'taskset 0,2,4,6 ptuse_ingest.py',\
              'docker_params': {"network":"host", "binds": {"/scratch":{"bind":"/data","ro":False}},
                  "devices":["/dev/nvidiactl:/dev/nvidiactl","/dev/nvidia-uvm:/dev/nvidia-uvm","/dev/nvidia0:/dev/nvidia0"],\
                  "privileged":True, "ulimits":[{"Name":"memlock","Soft":33554432,"Hard":-1}],\
