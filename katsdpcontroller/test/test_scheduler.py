e70f116b8112f77da7d2a0e8a7fc1d4c4819d711
Bruce Merry <bmerry@ska.ac.za>
2016-11-14 21:43:35 +0200

    Unit tests for TaskIDAllocator and Agent

--- katsdpcontroller/test/test_scheduler.py
+++ katsdpcontroller/test/test_scheduler.py
@@ -4,12 +4,19 @@ from nose.tools import *
 from katsdpcontroller import scheduler
 from katsdpcontroller.scheduler import TaskState
 from mesos.interface import mesos_pb2
+import uuid
+import ipaddress
+import logging
 import six
 import contextlib
 import functools
 import socket
 import trollius
 from trollius import From
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
 
 
 def run_with_event_loop(func):
@@ -157,3 +164,162 @@ class TestTaskState(object):
                 TaskState.RUNNING <= 3
             with assert_raises(NotImplementedError):
                 TaskState.RUNNING >= 3
+
+
+class TestTaskIDAllocator(object):
+    """Tests for :class:`katsdpcontroller.scheduler.TaskIDAllocator`."""
+    def test_singleton(self):
+        """Allocators with the same prefix are the same object"""
+        a = scheduler.TaskIDAllocator('test-foo-')
+        b = scheduler.TaskIDAllocator('test-bar-')
+        c = scheduler.TaskIDAllocator('test-foo-')
+        assert_is(a, c)
+        assert_is_not(a, b)
+
+    def test_call(self):
+        a = scheduler.TaskIDAllocator('test-baz-')
+        tid0 = a()
+        tid1 = a()
+        assert_equal('test-baz-00000000', tid0)
+        assert_equal('test-baz-00000001', tid1)
+
+
+class TestAgent(unittest.TestCase):
+    """Tests for :class:`katsdpcontroller.scheduler.Agent`.
+
+    This imports from :class:`unittest.TestCase` so that we can use
+    ``assertLogs``, which has not been ported to :mod:`nose.tools` yet."""
+    @classmethod
+    def _make_text_attr(cls, name, value):
+        attr = mesos_pb2.Attribute()
+        attr.name = name
+        attr.type = mesos_pb2.Value.SCALAR
+        attr.text.value = value
+        return attr
+
+    def _make_offer(self, resources, attrs):
+        offer = mesos_pb2.Offer()
+        offer.id.value = uuid.uuid4().hex
+        offer.framework_id.value = self.framework_id
+        offer.slave_id.value = self.slave_id
+        offer.hostname = self.host
+        for name, value in six.iteritems(resources):
+            resource = offer.resources.add()
+            resource.name = name
+            if isinstance(value, float):
+                resource.type = mesos_pb2.Value.SCALAR
+                resource.scalar.value = value
+            else:
+                resource.type = mesos_pb2.Value.RANGES
+                for start, stop in value:
+                    rng = resource.ranges.range.add()
+                    rng.begin = start
+                    rng.end = stop - 1
+        offer.attributes.extend(attrs)
+        return offer
+
+    def setUp(self):
+        self.slave_id = 'slaveid'
+        self.host = 'slavehostname'
+        self.framework_id = 'framework'
+        self.if_attr = self._make_text_attr(
+            'interface_eth0',
+            '{"name": "eth0", "network": "net0", "ipv4_address": "192.168.254.254"}')
+        self.if_attr_bad_json = self._make_text_attr(
+            'interface_bad_json',
+            '{not valid json')
+        self.if_attr_bad_schema = self._make_text_attr(
+            'interface_bad_schema',
+            '{"name": "eth1"}')
+
+
+    def test_construct(self):
+        """Construct an agent from some offers"""
+        attrs = [self.if_attr]
+        offers = [
+            self._make_offer({'cpus': 4.0, 'mem': 1024.0,
+                         'ports': [(100, 200), (300, 350)], 'cores': [(0, 8)]}, attrs),
+            self._make_offer({'cpus': 0.5, 'mem': 123.5, 'gpus': 1.0,
+                              'cores': [(8, 9)]}, attrs)
+        ]
+        agent = scheduler.Agent(offers)
+        assert_equal(self.slave_id, agent.slave_id)
+        assert_equal(self.host, agent.host)
+        assert_equal(len(attrs), len(agent.attributes))
+        for attr, agent_attr in zip(attrs, agent.attributes):
+            assert_equal(attr, agent_attr)
+        assert_equal(4.5, agent.cpus)
+        assert_equal(1147.5, agent.mem)
+        assert_equal(1.0, agent.gpus)
+        assert_equal(list(range(0, 9)), list(agent.cores))
+        assert_equal(list(range(100, 200)) + list(range(300, 350)), list(agent.ports))
+        assert_equal([scheduler.Interface(name='eth0',
+                                          network='net0',
+                                          ipv4_address=ipaddress.IPv4Address(u'192.168.254.254'),
+                                          numa_node=None)],
+                     agent.interfaces)
+
+    def test_bad_json(self):
+        """A warning must be printed if an interface description is not valid JSON"""
+        offers = [self._make_offer({}, [self.if_attr_bad_json])]
+        with self.assertLogs('katsdpcontroller.scheduler', logging.WARN):
+            agent = scheduler.Agent(offers)
+        assert_equal([], agent.interfaces)
+
+    def test_bad_schema(self):
+        """A warning must be printed if an interface description does not conform to the schema"""
+        offers = [self._make_offer({}, [self.if_attr_bad_schema])]
+        with self.assertLogs('katsdpcontroller.scheduler', logging.WARN):
+            agent = scheduler.Agent(offers)
+        assert_equal([], agent.interfaces)
+
+    def test_allocate_not_valid(self):
+        """allocate raises if the task does not accept the agent"""
+        task = scheduler.LogicalTask('task')
+        task.valid_agent = lambda x: False
+        agent = scheduler.Agent([self._make_offer({}, [])])
+        with assert_raises(scheduler.InsufficientResourcesError):
+            agent.allocate(task)
+
+    def test_allocate_insufficient_scalar(self):
+        """allocate raises if the task requires too much of a scalar resource"""
+        task = scheduler.LogicalTask('task')
+        task.cpus = 4.0
+        agent = scheduler.Agent([self._make_offer({'cpus': 2.0}, [])])
+        with assert_raises(scheduler.InsufficientResourcesError):
+            agent.allocate(task)
+
+    def test_allocate_insufficient_range(self):
+        """allocate raises if the task requires too much of a range resource"""
+        task = scheduler.LogicalTask('task')
+        task.cores = [None] * 3
+        agent = scheduler.Agent([self._make_offer({'cores': [(4, 6)]}, [])])
+        with assert_raises(scheduler.InsufficientResourcesError):
+            agent.allocate(task)
+
+    def test_allocate_missing_network(self):
+        """allocate raises if the task requires a network that is not present"""
+        task = scheduler.LogicalTask('task')
+        task.networks = ['net0', 'net1']
+        agent = scheduler.Agent([self._make_offer({}, [self.if_attr])])
+        with assert_raises(scheduler.InsufficientResourcesError):
+            agent.allocate(task)
+
+    def test_allocate_success(self):
+        """Tests allocate in the success case"""
+        task = scheduler.LogicalTask('task')
+        task.cpus = 4.0
+        task.mem = 128.0
+        task.cores = ['a', 'b', 'c']
+        task.networks = ['net0']
+        agent = scheduler.Agent([
+            self._make_offer({'cpus': 4.0, 'mem': 200.0, 'cores': [(4, 8)]}, [self.if_attr])])
+        ra = agent.allocate(task)
+        assert_equal(0.0, ra.gpus)
+        assert_equal(4.0, ra.cpus)
+        assert_equal(128.0, ra.mem)
+        assert_equal([4, 5, 6], ra.cores)
+        assert_equal(0.0, agent.cpus)
+        assert_equal(72.0, agent.mem)
+        assert_equal(0.0, agent.gpus)
+        assert_equal([7], list(agent.cores))
