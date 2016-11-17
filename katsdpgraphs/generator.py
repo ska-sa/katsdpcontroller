c63405334017b7f4e642b75547ccc0c7eb82c331
Simon Ratcliffe <simonr@ska.ac.za>
2016-11-17 10:09:18 +0200

    Include fengine stream

--- katsdpgraphs/generator.py
+++ katsdpgraphs/generator.py
@@ -22,12 +22,15 @@ def build_physical_graph(beamformer_mode, cbf_channels, simulate, resources):
     c_stream = 'c856M{}k_spead'.format(cbf_channels // 1024)
     telstate = '{}:{}'.format(r.get_host_ip('sdpmc'), r.get_port('redis'))
 
-    stream = "{}:'visibility'".format(c_stream)
+    streams = "{}:visibility".format(c_stream)
      # string containing a mapping from stream_name to stream_type.
      # This is temporary for AR1/1.5 and should be replaced by a
      # per stream sensor indicating type directly from the CBF
     if beamformer_mode not 'none':
-        stream_types += ",beam_0x:beamformer,beam_0y:beamformer"
+        streams += ",beam_0x:beamformer,beam_0y:beamformer"
+     # we also include a reference to the fengine stream so
+     # we can get n_samples_between_spectra
+    streams += ",fengine_stream:fengine"
 
     G = nx.DiGraph()
 
