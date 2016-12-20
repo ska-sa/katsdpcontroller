460caf907c222dcddd929c9e2ac0cfbe307a204d
Simon Ratcliffe <simonr@ska.ac.za>
2016-12-20 09:11:50 +0200

    Protect shutdown from further signals

--- scripts/sdp_master_controller.py
+++ scripts/sdp_master_controller.py
@@ -22,9 +22,14 @@ except ImportError:
 
 @tornado.gen.coroutine
 def on_shutdown(ioloop, server):
+    si = signal.signal(signal.SIGINT, signal.SIG_IGN)
+    st = signal.signal(signal.SIGTERM, signal.SIG_IGN)
+     # avoid any further interruptions whilst we handle shutdown
     server.handle_exit()
     yield server.stop()
     ioloop.stop()
+    signal.signal(signal.SIGINT, si)
+    signal.signal(signal.SIGTERM, st)
 
 if __name__ == "__main__":
 
