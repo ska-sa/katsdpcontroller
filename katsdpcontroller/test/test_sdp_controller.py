60d70574342e40c2971ed6ae0e6bbba8ec9a5623
Bruce Merry <bmerry@ska.ac.za>
2017-10-18 15:21:08 +0200

    Fix unit test

--- katsdpcontroller/test/test_sdp_controller.py
+++ katsdpcontroller/test/test_sdp_controller.py
@@ -657,6 +657,9 @@ class TestSDPController(unittest.TestCase):
             mock.call(Message.request('configure-subarray-from-telstate')),
             mock.call(Message.request('capture-init'), timeout=mock.ANY),
             mock.call(Message.request('capture-init'), timeout=mock.ANY),
+            mock.call(Message.request('capture-init'), timeout=mock.ANY),
+            mock.call(Message.request('capture-init'), timeout=mock.ANY),
+            mock.call(Message.request('capture-init'), timeout=mock.ANY),
             mock.call(Message.request(
                 'capture-start', 'i0_baseline_correlation_products'), timeout=mock.ANY)
         ])
