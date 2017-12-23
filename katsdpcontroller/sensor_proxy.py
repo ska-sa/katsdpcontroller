0b5e71482aa11b9fb5f8004d2e7f37119555e982
Bruce Merry <bmerry@gmail.com>
2017-12-23 15:01:21 +0200

    More work on porting SensorProxyClient to Python 3
    The main addition is the forgotten #sensor-status inform handler to
    actually update sensor data. There is also a fixup to be able to handle
    arbitrary strings as discrete values.

--- katsdpcontroller/sensor_proxy.py
+++ katsdpcontroller/sensor_proxy.py
@@ -65,6 +65,12 @@ class PrometheusObserver(object):
         self._status_metric.set(aiokatcp.Sensor.Status.UNREACHABLE)
 
 
+class DiscreteMixin:
+    @property
+    def katcp_value(self):
+        return self.value
+
+
 class SensorProxyClient(aiokatcp.Client):
     """Client that mirrors sensors into a device server,
     and optionally into Prometheus gauges as well.
@@ -105,6 +111,7 @@ class SensorProxyClient(aiokatcp.Client):
         # a tuple of names.
         self._enum_cache = {}
         self._sampling_set = set()   # Sensors whose sampling strategy has been set
+        self._synced = asyncio.Event(loop=self.loop)
 
     def qualify_name(self, name):
         """Map a remote server sensor name to a local name"""
@@ -130,11 +137,15 @@ class SensorProxyClient(aiokatcp.Client):
     def _make_type(self, type_name, parameters):
         """Get the sensor type for a given type name"""
         if type_name == 'discrete':
-            values = tuple(aiokatcp.decode(str, arg) for arg in parameters)
+            values = tuple(parameters)
             if values in self._enum_cache:
                 return self._enum_cache[values]
             else:
-                stype = enum.Enum('discrete', values)
+                # We need unique Python identifiers for each value, but simply
+                # normalising names in some way doesn't guarantee that.
+                # Instead, we use arbitrary numbering.
+                enums = [('ENUM{}'.format(i), value) for i, value in enumerate(values)]
+                stype = enum.Enum('discrete', enums, type=DiscreteMixin)
                 self._enum_cache[values] = stype
                 return stype
         else:
@@ -153,11 +164,13 @@ class SensorProxyClient(aiokatcp.Client):
             logger.exception('Exception in update task')
 
     def _trigger_update(self):
+        logger.debug('Sensor sync triggered')
+        self._synced.clear()
         if self._update_task is not None:
             self._update_task.cancel()
             self._update_task = None
         self._update_task = self.loop.create_task(self._update())
-        self._update_task.set_done_callback(self._update_done)
+        self._update_task.add_done_callback(self._update_done)
 
     def _add_sensor(self, name, sensor):
         """Add a new or replaced sensor with unqualified name `name`."""
@@ -199,7 +212,8 @@ class SensorProxyClient(aiokatcp.Client):
                 name, description, units, type_name = [aiokatcp.decode(str, inform.arguments[i])
                                                        for i in range(4)]
                 if type_name not in SENSOR_TYPES:
-                    logger.warning('Type %s is not recognised, skipping sensor %s', type_name, name)
+                    logger.warning('Type %s is not recognised, skipping sensor %s',
+                                        type_name, name)
                     continue
                 full_name = self.qualify_name(name)
                 stype = self._make_type(type_name, inform.arguments[4:])
@@ -221,6 +235,7 @@ class SensorProxyClient(aiokatcp.Client):
                     self._remove_sensor(name)
                     changed = True
             await self._set_sampling(sampling)
+            self._synced.set()
         finally:
             if changed:
                 self.server.mass_inform('interface-changed', 'sensor-list')
@@ -230,6 +245,7 @@ class SensorProxyClient(aiokatcp.Client):
         self._trigger_update()
 
     def __disconnected(self):
+        self._synced.clear()
         if self._update_task is not None:
             self._update_task.cancel()
             self._update_task = None
@@ -241,10 +257,32 @@ class SensorProxyClient(aiokatcp.Client):
         # This could eventually be smarter and consult the args
         self._trigger_update()
 
+    def inform_sensor_status(self, timestamp: aiokatcp.Timestamp, n: int, *args) -> None:
+        if len(args) != 3 * n:
+            raise aiokatcp.FailReply('Incorrect number of arguments')
+        for i in range(n):
+            name = '<unknown>'
+            try:
+                name = aiokatcp.decode(str, args[3 * i])
+                status = aiokatcp.decode(aiokatcp.Sensor.Status, args[3 * i + 1])
+                try:
+                    sensor = self.server.sensors[self.qualify_name(name)]
+                except KeyError:
+                    logger.info('Received update for unknown sensor %s', name)
+                    continue
+                value = aiokatcp.decode(sensor.stype, args[3 * i + 2])
+                sensor.set_value(value, status=status, timestamp=timestamp)
+            except Exception:
+                logger.warning('Failed to process #sensor-status for %s', name, exc_info=True)
+
+    async def wait_synced(self):
+        await self._synced.wait()
+
     def close(self):
         if self._update_task is not None:
             self._update_task.cancel()
             self._update_task = None
+        self._synced.clear()
         if self._observers:
             self.server.mass_inform('interface-changed', 'sensor-list')
         for name in list(self._observers.keys()):
