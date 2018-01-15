57f7f699402c2a74b77b55393f0c4b8e73c7aa58
Bruce Merry <bmerry@ska.ac.za>
2018-01-15 14:39:43 +0200

    Change python -> python3 in setup.py shebang line

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from setuptools import setup, find_packages
 
 # Package dependencies are handled externally via requirements.txt to please Docker and Travis
