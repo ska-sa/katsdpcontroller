257cfe71736d5ecb5720a04e705d2d68b0345c26
Bruce Merry <bmerry@ska.ac.za>
2019-01-22 09:56:29 +0200

    Update config for beamformer-engineering
    This depends on ska-sa/katsdpingest#290.
    
    - Remove --max-packet-size heuristic. The code now uses multi-packet
    receive queues where performance is not sensitive to the max packet
    size.
    - Remove --buffer-size argument. The constraint that made it necessary
    to calculate it disappears with multi-packet receive queues, so the
    default should be fine.
    - Change buffer space calculation to match the code.

--- katsdpcontroller/generator.py
+++ katsdpcontroller/generator.py
@@ -1285,10 +1285,9 @@ def _make_beamformer_engineering_pol(g, info, node_name, src_name, timeplot, ram
     bf_ingest.cores = ['disk', 'network']
     bf_ingest.capabilities.append('SYS_NICE')
     if timeplot or not ram:
-        # bf_ingest accumulates 512 frames in the ring buffer. It's not a
-        # lot of memory, so to be on the safe side we double everything.
-        # Values are int8*2.  Allow 512MB for various buffers.
-        bf_ingest.mem = 1024 * _mb(info.size) + 512
+        # Actual usage is about 600MB, more-or-less independent of the
+        # parameters.
+        bf_ingest.mem = 1024
     else:
         # When writing to tmpfs, the file is accounted as memory to our
         # process, so we need more memory allocation than there is
@@ -1311,19 +1310,11 @@ def _make_beamformer_engineering_pol(g, info, node_name, src_name, timeplot, ram
     bf_ingest.transitions = CAPTURE_TRANSITIONS
 
     def make_beamformer_engineering_pol_config(task, resolver):
-        # Temporary until CBF provide a packet size sensor (CBFTASKS-748)
-        # Size refers to SPEAD packet, so includes payload plus ~64 bytes of headers
-        if src_info.n_channels == 1024 and src_info.n_channels_per_substream == 4:
-            max_packet = 1100
-        else:
-            max_packet = 4200
         config = {
             'affinity': [task.cores['disk'], task.cores['network']],
             'interface': task.interfaces['cbf'].name,
             'ibv': not develop,
-            'stream_name': src_name,
-            'max_packet': max_packet,
-            'buffer_size': max_packet * 32767   # Maximum we can use on ConnectX-5
+            'stream_name': src_name
         }
         if timeplot:
             config.update({
