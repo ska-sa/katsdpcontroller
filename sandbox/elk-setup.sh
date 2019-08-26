924217f26e0876628a31c128ba32f497c1160967
Bruce Merry <bmerry@ska.ac.za>
2019-08-26 14:59:53 +0200

    Add in a full ELK stack
    It includes a shell script to poke ES and Kibana through curl to create
    their config.

---
+++ sandbox/elk-setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e -u
+
+trap 'echo Failed' ERR
+
+echo "Setting Elasticsearch pipeline"
+curl -s --fail -X PUT -H 'Content-type: application/json' -d '@etc/elk/elasticsearch-pipeline.json' 'http://localhost:9200/_ingest/pipeline/timestamp_precise' > /dev/null
+echo "Setting Kibana settings"
+curl -s --fail -X POST -H 'Content-type: application/json' -H 'kbn-xsrf: true' -d '@etc/elk/kibana-settings.json' 'http://localhost:5601/api/saved_objects/config/7.3.1?overwrite=true' > /dev/null
+echo "Setting Kibana index pattern"
+curl -s --fail -X POST -H 'Content-type: application/json' -H 'kbn-xsrf: true' -d '@etc/elk/kibana-index-pattern.json' 'http://localhost:5601/api/saved_objects/index-pattern/logstash?overwrite=true' > /dev/null
+echo "Setting Kibana search"
+curl -s --fail -X POST -H 'Content-type: application/json' -H 'kbn-xsrf: true' -d '@etc/elk/sdp-messages.json' 'http://localhost:5601/api/saved_objects/search/sdp-messages?overwrite=true' > /dev/null
+
+echo
+echo "Success"
