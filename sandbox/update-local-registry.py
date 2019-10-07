3750ae295ba1198f3e52e60e08470229674ac908
Bruce Merry <bmerry@ska.ac.za>
2019-10-07 10:02:44 +0200

    Refactoring of update-local-registry script
    - All info about an image collecting in ImageInfo class
    - Added --include and --exclude options

--- sandbox/update-local-registry.py
+++ sandbox/update-local-registry.py
@@ -1,42 +1,101 @@
 #!/usr/bin/env python3
 
+import enum
 import subprocess
 import argparse
 import tempfile
 import shutil
 import os
+import logging
+from typing import List, Set, Iterable, Optional
 
-from katsdpcontroller.generator import IMAGES
+import katsdpcontroller.generator
 
 
-EXTRA_COPY = ['katsdpcontroller', 'docker-base-runtime', 'docker-base-build']
-MANUAL = ['docker-base-gpu-build', 'docker-base-gpu-runtime',
-          'katsdpingest', 'katsdpingest_titanx',
-          'katcbfsim', 'katsdpcontim', 'katsdpimager',
-          # Images below here should eventually exist on quay.io
-          'katsdpcal', 'katsdpcontroller']
-REPO_MAP = {
-    'docker-base-gpu-build': 'katsdpdockerbase',
-    'docker-base-gpu-runtime': 'katsdpdockerbase',
-    'katsdpcal': 'katsdppipelines',
-    'katsdpcontim': 'katsdppipelines'
+class Action(enum.Enum):
+    COPY = 1
+    BUILD = 2
+    TUNE = 3
+
+
+class ImageInfo:
+    def __init__(self, *,
+                 action: Action = Action.COPY,
+                 repo: Optional[str] = None,
+                 branch: str = 'master'):
+        self.action = action
+        self.repo = repo
+        self.branch = branch
+
+
+# Order is important here: images are built in order
+EXTRA_IMAGES = ['docker-base-runtime', 'docker-base-build',
+                'docker-base-gpu-build', 'docker-base-gpu-runtime',
+                'katsdpingest']
+IMAGES = EXTRA_IMAGES + list(katsdpcontroller.generator.IMAGES)
+
+IMAGE_INFO = {
+    'docker-base-runtime': ImageInfo(repo='katsdpdockerbase'),
+    'docker-base-build': ImageInfo(repo='katsdpdockerbase'),
+    'docker-base-gpu-build': ImageInfo(action=Action.BUILD, repo='katsdpdockerbase'),
+    'docker-base-gpu-runtime': ImageInfo(action=Action.BUILD, repo='katsdpdockerbase'),
+    'katsdpcal': ImageInfo(action=Action.BUILD, repo='katsdppipelines'),
+    'katsdpcontim': ImageInfo(action=Action.BUILD, repo='katsdppipelines'),
+    'katsdpingest_titanx': ImageInfo(action=Action.TUNE, repo='katsdpingest'),
+    'katsdpingest': ImageInfo(action=Action.BUILD),
+    'katsdpimager': ImageInfo(action=Action.BUILD, branch='more-packaging'),
+    'katcbfsim': ImageInfo(action=Action.BUILD),
+    # Will go away after merge
+    'katsdpcontroller': ImageInfo(action=Action.BUILD, branch='image-builder')
 }
-# Branches to build instead of master. These should be temporary, for cases
-# where repositories don't yet build outside of SARAO.
-BRANCH_MAP = {
-    'katsdpimager': 'more-packaging',
-    'katsdpcontroller': 'image-builder'
-}
-SPECIAL = frozenset({'katsdpingest_titanx'})
+
+
+def image_info(name: str) -> ImageInfo:
+    return IMAGE_INFO.get(name, ImageInfo())
+
+
+def expand_special(names: Iterable[str]) -> Set[str]:
+    out: Set[str] = set()
+    for name in names:
+        if name == 'all':
+            out |= set(IMAGES)
+        elif name == 'copy':
+            out |= set(image for image in IMAGES if image_info(image).action == Action.COPY)
+        elif name == 'build':
+            out |= set(image for image in IMAGES if image_info(image).action == Action.BUILD)
+        elif name == 'tune':
+            out |= set(image for image in IMAGES if image_info(image).action == Action.TUNE)
+        elif name == 'none':
+            pass
+        else:
+            if name not in IMAGES:
+                logging.warning('Image %s not known', name)
+            out.add(name)
+    return out
+
+
+def comma_split(x: str) -> List[str]:
+    if not x:
+        return []
+    else:
+        return x.split(',')
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument('--upstream', help='upstream registry from which to pull pre-built images',
-                        default='quay.io/ska-sa')
-    parser.add_argument('--downstream', help='registry to push images into',
-                        default='localhost:5000')
+    parser.add_argument('--upstream', default='quay.io/ska-sa',
+                        help='upstream registry from which to pull pre-built images [%(default)s]')
+    parser.add_argument('--downstream', default='localhost:5000',
+                        help='registry to push images into [%(default)s]')
+    parser.add_argument('--build-all', action='store_true',
+                        help='Build all images rather than copying from upstream')
+    parser.add_argument('--include', type=comma_split, default=['all'],
+                        help='comma-separated list of images to update [all]')
+    parser.add_argument('--exclude', type=comma_split, default=[],
+                        help='comma-separated list of images to skip [none]')
     args = parser.parse_args()
+    args.include = expand_special(args.include)
+    args.exclude = expand_special(args.exclude)
     return args
 
 
@@ -53,12 +112,13 @@ def docker_cmd(*args: str) -> None:
 
 
 def build_image(name: str, args: argparse.Namespace) -> None:
+    info = image_info(name)
     tmpdir = tempfile.mkdtemp()
     try:
-        repo = REPO_MAP.get(name, name)
+        repo = info.repo or name
         url = f'https://github.com/ska-sa/{repo}'
-        branch = BRANCH_MAP.get(name, 'master')
-        print(f'Checking out {url} branch {branch} to build {repo}')
+        branch = info.branch
+        logging.info('Checking out %s branch %s to build %s', url, branch, repo)
         # GIT_TERMINAL_PROMPT=0 prevents Git from asking for credentials,
         # which could happen if a typo meant the repository did not exist.
         subprocess.run(
@@ -76,8 +136,8 @@ def build_image(name: str, args: argparse.Namespace) -> None:
             encoding='utf-8').stdout.strip()
 
         downstream_image = downstream_path(name, args)
-        print(f'Building image {downstream_image}')
-        workdir = os.path.join(tmpdir, name) if name in REPO_MAP else tmpdir
+        logging.info('Building image %s', downstream_image)
+        workdir = os.path.join(tmpdir, name) if info.repo else tmpdir
         if os.path.exists(os.path.join(workdir, 'scripts', 'docker_build.sh')):
             docker_build = ['scripts/docker_build.sh']
         else:
@@ -92,9 +152,9 @@ def build_image(name: str, args: argparse.Namespace) -> None:
                 '--pull=true',
                 '-t', downstream_image, '.'
             ], stdin=subprocess.DEVNULL, check=True, cwd=workdir)
-        print(f'Pushing {downstream_image}')
+        logging.info('Pushing %s', downstream_image)
         docker_cmd('push', downstream_image)
-        print(f'{downstream_image} pushed')
+        logging.info('%s pushed', downstream_image)
     finally:
         shutil.rmtree(tmpdir)
 
@@ -102,24 +162,29 @@ def build_image(name: str, args: argparse.Namespace) -> None:
 def copy_image(name: str, args: argparse.Namespace) -> None:
     upstream_image = upstream_path(name, args)
     downstream_image = downstream_path(name, args)
-    print(f'Pulling {upstream_image}')
+    logging.info('Pulling %s', upstream_image)
     docker_cmd('pull', upstream_image)
-    print(f'Tagging {upstream_image} -> {downstream_image}')
+    logging.info('Tagging %s -> %s', upstream_image, downstream_image)
     docker_cmd('tag', upstream_image, downstream_image)
-    print(f'Pushing {downstream_image}')
+    logging.info('Pushing %s', downstream_image)
     docker_cmd('push', downstream_image)
-    print(f'{downstream_image} pushed')
+    logging.info('%s pushed', downstream_image)
 
 
 def main() -> None:
+    logging.basicConfig(level='INFO')
     args = parse_args()
-    names = sorted(IMAGES) + EXTRA_COPY
-    for name in names:
-        if name not in MANUAL:
-            copy_image(name, args)
-    for name in MANUAL:
-        if name not in SPECIAL:
+
+    for name in IMAGES:
+        if name not in args.include or name in args.exclude:
+            continue
+        info = image_info(name)
+        if info.action == Action.TUNE:
+            logging.warning('Autotuning not implemented yet (%s)', name)
+        elif info.action == Action.BUILD or args.build_all:
             build_image(name, args)
+        else:
+            copy_image(name, args)
 
 
 if __name__ == '__main__':
