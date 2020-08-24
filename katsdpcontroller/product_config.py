94aca732dc9dcf5d8db57854a8b4bf2293c2136b
Bruce Merry <bmerry@ska.ac.za>
2020-08-24 11:27:24 +0200

    Get rid of uselessly redefined properties
    They're defined exactly the same on the base class.

--- katsdpcontroller/product_config.py
+++ katsdpcontroller/product_config.py
@@ -1150,14 +1150,6 @@ class SpectralImageStream(ImageStream):
         return self.output_channels[1] - self.output_channels[0]
 
     @property
-    def flags(self) -> FlagsStream:
-        return cast(FlagsStream, self.src_streams[0])
-
-    @property
-    def vis(self) -> VisStream:
-        return self.flags.vis
-
-    @property
     def continuum(self) -> Optional[ContinuumImageStream]:
         try:
             return cast(ContinuumImageStream, self.src_streams[1])
