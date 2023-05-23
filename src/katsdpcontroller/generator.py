ae24aa80dcb245aefccc8555e608e4cc02e977c6
Bruce Merry <bmerry@sarao.ac.za>
2023-05-23 09:44:58 +0200

    Allocate more GPU memory for fgpu
    The new narrowband approach uses more.

--- src/katsdpcontroller/generator.py
+++ src/katsdpcontroller/generator.py
@@ -740,7 +740,7 @@ def _make_fgpu(
         fgpu.image = "katgpucbf"
         fgpu.fake_katcp_server_cls = FakeFgpuDeviceServer
         fgpu.cpus = 4
-        fgpu.mem = 1200  # Actual use is currently around 1GB when using narrowband
+        fgpu.mem = 1700  # Actual use is currently around 1.6GB when using narrowband
         if not configuration.options.develop.less_resources:
             fgpu.cores = ["src0", "src1", "dst", "python"]
             fgpu.numa_nodes = 1.0  # It's easily starved of bandwidth
