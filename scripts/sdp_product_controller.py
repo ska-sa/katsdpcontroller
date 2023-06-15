774790ad754b45b8e4350f16a3fd01ca733673a0
Bruce Merry <bmerry@sarao.ac.za>
2023-06-15 08:31:49 +0200

    Add copyright headers to source files
    These somehow never got added. Rather than trying to trace the history
    in each file (including tracing code through refactorings) I just marked
    everything as copyright 2013-2023.

--- scripts/sdp_product_controller.py
+++ scripts/sdp_product_controller.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
 
-"""Script for launching the Science Data Processor Subarray Product Controller.
+################################################################################
+# Copyright (c) 2013-2023, National Research Foundation (SARAO)
+#
+# Licensed under the BSD 3-Clause License (the "License"); you may not use
+# this file except in compliance with the License. You may obtain a copy
+# of the License at
+#
+#   https://opensource.org/licenses/BSD-3-Clause
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
 
-   Copyright (c) 2013 SKA/KAT. All Rights Reserved.
-"""
+"""Script for launching the Science Data Processor Subarray Product Controller."""
 
 import argparse
 import asyncio
