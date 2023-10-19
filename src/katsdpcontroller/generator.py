3049e23a1c3c6eb5c1c0373ef58baa21a55e6497
Ludwig Schwardt <ludwig@ska.ac.za>
2023-10-19 13:26:09 +0200

    SPR1-2999 Prefix bf_ingest command with capambel
    This change is motivated by a change in the docker daemon and the way
    linux capabilities are handled.
    
    CAP_NET_RAW capability is transferred to bf_ingest by capambel.c
    
    This is a continuation of #658.

--- src/katsdpcontroller/generator.py
+++ src/katsdpcontroller/generator.py
@@ -2320,6 +2320,9 @@ def _make_beamformer_engineering_pol(
     bf_ingest.cpus = 2
     bf_ingest.cores = ["disk", "network"]
     bf_ingest.capabilities.append("SYS_NICE")
+    if ibv:
+        bf_ingest.command = ["capambel", "-c", "cap_net_raw+p", "--"] + bf_ingest.command
+        bf_ingest.capabilities.append("NET_RAW")  # For ibverbs raw QPs
     if timeplot or not ram:
         # Actual usage is about 600MB, more-or-less independent of the
         # parameters.
