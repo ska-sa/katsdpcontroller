3bd43598cf5096bb7d575207ff9d4d47b65b7693
Bruce Merry <bmerry@sarao.ac.za>
2025-03-11 21:08:40 +0200

    Normalise stream name for band mask/primary beam models
    Contributes to NGC-1551.

--- src/katsdpcontroller/product_controller.py
+++ src/katsdpcontroller/product_controller.py
@@ -1638,7 +1638,8 @@ class SubarrayProduct:
                         model_base_url,
                         f"band_mask/current/{stream.band}/nb_ratio={ratio}.alias",
                     )
-                    prefix: Tuple[str, ...] = (stream.name, "model", "band_mask")
+                    stream_name_norm = stream.name.replace("-", "_")
+                    prefix: Tuple[str, ...] = (stream_name_norm, "model", "band_mask")
                     init_telstate[prefix + ("config",)] = band_mask_model_urls[0]
                     init_telstate[prefix + ("fixed",)] = band_mask_model_urls[1]
                     for group in ["individual", "cohort"]:
@@ -1652,7 +1653,7 @@ class SubarrayProduct:
                             )
                             config_value[ant] = pb_model_urls[0]
                             fixed_value[ant] = pb_model_urls[1]
-                        prefix = (stream.name, "model", "primary_beam", group)
+                        prefix = (stream_name_norm, "model", "primary_beam", group)
                         init_telstate[prefix + ("config",)] = config_value
                         init_telstate[prefix + ("fixed",)] = fixed_value
 
